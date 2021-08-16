26682efa8d520d012ea32870cf46df9aa572b31c
Abdullah Mansoor <abdullahmansoor277@gmail.com>
2021-08-16 16:29:49 +0500

    first commit task tracker

---
+++ src/components/Task.vue
@@ -0,0 +1,39 @@
+<template>
+	<div
+		@dblclick="this.$emit('toggle-reminder', id)"
+		:class="[task.reminder ? 'reminder' : '', 'task']"
+	>
+		<h3>
+			{{ task.text }}
+			<i @click="this.$emit('delete-task', id)" class="fas fa-times"></i>
+		</h3>
+		<p>{{ task.day }}</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Task",
+	props: { task: Object },
+};
+</script>
+
+<style scoped>
+.fas {
+	color: red;
+}
+.task {
+	background: #f4f4f4;
+	margin: 5px;
+	padding: 10px 20px;
+	cursor: pointer;
+}
+.task.reminder {
+	border-left: 5px solid green;
+}
+.task h3 {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+}
+</style>
